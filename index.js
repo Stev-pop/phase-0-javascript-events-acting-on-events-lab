ffb89c44ef582df748d2b950e714e01e988fe478
Stephen Onyango <onyangosteve01@gmail.com>
2024-04-03 00:38:21 +0300

    Adds assignment to memory

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+//Declare global variable
+const dodger = document.getElementById("dodger");
+
+//moves dodger to the left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  //Move dodger to the right
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
